40222aebbf6d927a162f708b192155069ea81e36
Modark <modark@Modark.Modark>
2025-10-12 16:41:03 -0400

    change wireless_attack_tools.py NEW(design)

--- tools/wireless_attack_tools.py
+++ tools/wireless_attack_tools.py
@@ -4,19 +4,29 @@ import os
 from core import HackingTool
 from core import HackingToolsCollection
 
+from rich.console import Console
+from rich.theme import Theme
+from rich.table import Table
+from rich.panel import Panel
+
+_theme = Theme({"purple": "#7B61FF"})
+console = Console(theme=_theme)
+
 
 class WIFIPumpkin(HackingTool):
     TITLE = "WiFi-Pumpkin"
-    DESCRIPTION = "The WiFi-Pumpkin is a rogue AP framework to easily create " \
-                  "these fake networks\n" \
-                  "all while forwarding legitimate traffic to and from the " \
-                  "unsuspecting target."
+    DESCRIPTION = (
+        "The WiFi-Pumpkin is a rogue AP framework to easily create "
+        "these fake networks\n"
+        "all while forwarding legitimate traffic to and from the "
+        "unsuspecting target."
+    )
     INSTALL_COMMANDS = [
         "sudo apt install libssl-dev libffi-dev build-essential",
         "sudo git clone https://github.com/P0cL4bs/wifipumpkin3.git",
         "chmod -R 755 wifipumpkin3",
         "sudo apt install python3-pyqt5",
-        "cd wifipumpkin3;sudo python3 setup.py install"
+        "cd wifipumpkin3;sudo python3 setup.py install",
     ]
     RUN_COMMANDS = ["sudo wifipumpkin3"]
     PROJECT_URL = "https://github.com/P0cL4bs/wifipumpkin3"
@@ -24,14 +34,16 @@ class WIFIPumpkin(HackingTool):
 
 class pixiewps(HackingTool):
     TITLE = "pixiewps"
-    DESCRIPTION = "Pixiewps is a tool written in C used to bruteforce offline " \
-                  "the WPS pin\n " \
-                  "exploiting the low or non-existing entropy of some Access " \
-                  "Points, the so-called pixie dust attack"
+    DESCRIPTION = (
+        "Pixiewps is a tool written in C used to bruteforce offline "
+        "the WPS pin\n "
+        "exploiting the low or non-existing entropy of some Access "
+        "Points, the so-called pixie dust attack"
+    )
     INSTALL_COMMANDS = [
         "sudo git clone https://github.com/wiire/pixiewps.git && apt-get -y install build-essential",
         "cd pixiewps*/ && make",
-        "cd pixiewps*/ && sudo make install && wget https://pastebin.com/y9Dk1Wjh"
+        "cd pixiewps*/ && sudo make install && wget https://pastebin.com/y9Dk1Wjh",
     ]
     PROJECT_URL = "https://github.com/wiire/pixiewps"
 
@@ -42,16 +54,19 @@ class pixiewps(HackingTool):
             '\'airmon-ng start {wireless interface}\n'
             '2.> wash -i {monitor-interface like mon0}\'\n'
             '3.> reaver -i {monitor interface} -b {BSSID of router} -c {router channel} -vvv -K 1 -f"'
-            '| boxes -d boy')
+            '| boxes -d boy'
+        )
         print("You Have To Run Manually By USing >>pixiewps -h ")
 
 
 class BluePot(HackingTool):
     TITLE = "Bluetooth Honeypot GUI Framework"
-    DESCRIPTION = "You need to have at least 1 bluetooth receiver " \
-                  "(if you have many it will work with those, too).\n" \
-                  "You must install/libbluetooth-dev on " \
-                  "Ubuntu/bluez-libs-devel on Fedora/bluez-devel on openSUSE"
+    DESCRIPTION = (
+        "You need to have at least 1 bluetooth receiver "
+        "(if you have many it will work with those, too).\n"
+        "You must install/libbluetooth-dev on "
+        "Ubuntu/bluez-libs-devel on Fedora/bluez-devel on openSUSE"
+    )
     INSTALL_COMMANDS = [
         "sudo wget https://raw.githubusercontent.com/andrewmichaelsmith/bluepot/master/bin/bluepot-0.2.tar.gz"
         "sudo tar xfz bluepot-0.2.tar.gz;sudo rm bluepot-0.2.tar.gz"
@@ -83,7 +98,7 @@ class Wifiphisher(HackingTool):
     """
     INSTALL_COMMANDS = [
         "git clone https://github.com/wifiphisher/wifiphisher.git",
-        "cd wifiphisher;sudo python3 setup.py install"
+        "cd wifiphisher;sudo python3 setup.py install",
     ]
     RUN_COMMANDS = ["cd wifiphisher;sudo wifiphisher"]
     PROJECT_URL = "https://github.com/wifiphisher/wifiphisher"
@@ -94,7 +109,7 @@ class Wifite(HackingTool):
     DESCRIPTION = "Wifite is an automated wireless attack tool"
     INSTALL_COMMANDS = [
         "sudo git clone https://github.com/derv82/wifite2.git",
-        "cd wifite2 && sudo python3 setup.py install"
+        "cd wifite2 && sudo python3 setup.py install",
     ]
     RUN_COMMANDS = ["cd wifite2; sudo wifite"]
     PROJECT_URL = "https://github.com/derv82/wifite2"
@@ -102,8 +117,10 @@ class Wifite(HackingTool):
 
 class EvilTwin(HackingTool):
     TITLE = "EvilTwin"
-    DESCRIPTION = "Fakeap is a script to perform Evil Twin Attack, by getting" \
-                  " credentials using a Fake page and Fake Access Point"
+    DESCRIPTION = (
+        "Fakeap is a script to perform Evil Twin Attack, by getting"
+        " credentials using a Fake page and Fake Access Point"
+    )
     INSTALL_COMMANDS = ["sudo git clone https://github.com/Z4nzu/fakeap.git"]
     RUN_COMMANDS = ["cd fakeap && sudo bash fakeap.sh"]
     PROJECT_URL = "https://github.com/Z4nzu/fakeap"
@@ -111,12 +128,14 @@ class EvilTwin(HackingTool):
 
 class Fastssh(HackingTool):
     TITLE = "Fastssh"
-    DESCRIPTION = "Fastssh is an Shell Script to perform multi-threaded scan" \
-                  " \n and brute force attack against SSH protocol using the " \
-                  "most commonly credentials."
+    DESCRIPTION = (
+        "Fastssh is an Shell Script to perform multi-threaded scan"
+        " \n and brute force attack against SSH protocol using the "
+        "most commonly credentials."
+    )
     INSTALL_COMMANDS = [
         "sudo git clone https://github.com/Z4nzu/fastssh.git && cd fastssh && sudo chmod +x fastssh.sh",
-        "sudo apt-get install -y sshpass netcat"
+        "sudo apt-get install -y sshpass netcat",
     ]
     RUN_COMMANDS = ["cd fastssh && sudo bash fastssh.sh --scan"]
     PROJECT_URL = "https://github.com/Z4nzu/fastssh"
@@ -124,12 +143,14 @@ class Fastssh(HackingTool):
 
 class Howmanypeople(HackingTool):
     TITLE = "Howmanypeople"
-    DESCRIPTION = "Count the number of people around you by monitoring wifi " \
-                  "signals.\n" \
-                  "[@] WIFI ADAPTER REQUIRED* \n[*]" \
-                  "It may be illegal to monitor networks for MAC addresses, \n" \
-                  "especially on networks that you do not own. " \
-                  "Please check your country's laws"
+    DESCRIPTION = (
+        "Count the number of people around you by monitoring wifi "
+        "signals.\n"
+        "[@] WIFI ADAPTER REQUIRED* \n[*]"
+        "It may be illegal to monitor networks for MAC addresses, \n"
+        "especially on networks that you do not own. "
+        "Please check your country's laws"
+    )
     INSTALL_COMMANDS = [
         "sudo apt-get install tshark"
         ";sudo python3 -m pip install howmanypeoplearearound"
@@ -149,5 +170,24 @@ class WirelessAttackTools(HackingToolsCollection):
         Wifite(),
         EvilTwin(),
         Fastssh(),
-        Howmanypeople()
+        Howmanypeople(),
     ]
+
+    def pretty_print(self):
+        table = Table(title="Wireless Attack Tools", show_lines=True, expand=True)
+        table.add_column("Title", style="purple", no_wrap=True)
+        table.add_column("Description", style="purple")
+        table.add_column("Project URL", style="purple", no_wrap=True)
+
+        for t in self.TOOLS:
+            desc = getattr(t, "DESCRIPTION", "") or ""
+            url = getattr(t, "PROJECT_URL", "") or ""
+            table.add_row(t.TITLE, desc.strip().replace("\n", " "), url)
+
+        panel = Panel(table, title="[purple]Available Tools[/purple]", border_style="purple")
+        console.print(panel)
+
+
+if __name__ == "__main__":
+    tools = WirelessAttackTools()
+    tools.pretty_print()
